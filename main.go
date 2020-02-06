448d9980eb34ea069ac048bd4b7cebe07492b212
Ola Nordstrom <ola.nordstrom@optimizely.com>
2020-02-05 16:46:55 -0800

    cleanup

--- main.go
+++ main.go
@@ -10,26 +10,19 @@ import (
 	"os"
 )
 
-var optimizelyFactory *client.OptimizelyFactory
-
 var optlyClient *client.OptimizelyClient
-
-//var optlyClient *client.OptimizelyClient
 var user *entities.UserContext
 
 // initalizes the Optimizely SDK
 //export Init
 func Init(sdkkey, userId string) {
-	optimizelyFactory = &client.OptimizelyFactory{SDKKey: "***REMOVED***"}
 	var err error
-	optlyClient, err = optly.Client("***REMOVED***")
-	//optlyClient, err = client.OptimizelyClient("***REMOVED***")
+	optlyClient, err = optly.Client(sdkkey)
 	if err != nil {
 		fmt.Printf("err: %s\n", err)
 		os.Exit(1)
 	}
 	user = &entities.UserContext{ID: userId}
-	//user := optly.UserContext(userId, nil)
 }
 
 // checks to see if a feature is Enabled
@@ -44,28 +37,9 @@ func IsFeatureEnabled(featureName string) bool {
 }
 
 func main() {
-	fmt.Println("vim-go")
-
 	Init("***REMOVED***", "0x123SomeUserId00")
 	enabled := IsFeatureEnabled("is_it_on_feature_key")
 	if enabled == true {
 		fmt.Printf("is_feature_enabled: %b\n", enabled)
 	}
-
-	/*
-		optimizelyFactory := &client.OptimizelyFactory{SDKKey: "***REMOVED***"}
-
-		optlyClient, err := optimizelyFactory.Client()
-		if err != nil {
-			fmt.Printf("err: %s\n", err)
-			os.Exit(1)
-		}
-
-		user := entities.UserContext{ID: "0x123SomeUserId00"}
-
-		enabled, _ := optlyClient.IsFeatureEnabled("is_it_on_feature_key", user)
-		if enabled == true {
-			fmt.Printf("is_feature_enabled: %b\n", enabled)
-		}
-	*/
 }
